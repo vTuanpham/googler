c01b660b59402077b6dcd54a0137d081ad2a442c
Tuan-Pham <weekend.2810@gmail.com>
2023-05-04 13:36:49 +0700

    feat: add github actions

---
+++ search_test.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import unittest
+import subprocess
+sys.path.insert(0,r'./') #Add root directory here
+
+
+class GoogleTest(unittest.TestCase):
+    def test_search_wiki(self):
+        # Change the current working directory
+        cwd = os.getcwd()
+        result = subprocess.run([".\google.exe", "Facebook"], capture_output=True, cwd=os.path.join(cwd,'dist'), shell=True)
+        with open('./test_example/Facebook_wiki.txt', 'r') as f:
+            facebook_wiki_ref = f.read()
+        self.assertIn(facebook_wiki_ref.lower(), str(result.stdout).lower())
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
