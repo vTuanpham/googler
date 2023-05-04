882e7e1e5af4736a96b269391a93fd932b570a2f
Tuan-Pham <weekend.2810@gmail.com>
2023-05-04 17:17:31 +0700

    fix: fix search_test.py

--- search_test.py
+++ search_test.py
@@ -7,10 +7,41 @@ sys.path.insert(0,r'./') #Add root directory here
 
 class GoogleTest(unittest.TestCase):
 
+    def test_code_success(self):
+        # Change the current working directory
+        cwd = os.getcwd()
+        print(cwd)
+        process = subprocess.Popen(["python", "google", "--help"],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       shell=True)
+
+        result, err = process.communicate()
+
+        with open('./test_example/python_help.txt', 'r') as f:
+            help_ref = f.read()
+        self.assertEqual(help_ref.lower(), str(result).lower())
+
+    def test_search_wiki(self):
+        # Change the current working directory
+        cwd = os.getcwd()
+        print(cwd)
+        process = subprocess.Popen(["python", "google", "Facebook"],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       shell=True)
+
+        result, err = process.communicate()
+
+        with open('./test_example/Facebook_wiki.txt', 'r') as f:
+            help_ref = f.read()
+        self.assertIn(help_ref.lower(), str(result).lower())
+
     def test_build_success(self):
         # Change the current working directory
         cwd = os.getcwd()
-        process = subprocess.Popen(["./google.exe", "--help"],
+        print(cwd)
+        process = subprocess.Popen(["google.exe", "--help"],
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE,
                                        cwd=os.path.join(cwd,'dist'), shell=True)
@@ -24,7 +55,8 @@ class GoogleTest(unittest.TestCase):
     def test_search_wiki(self):
         # Change the current working directory
         cwd = os.getcwd()
-        process = subprocess.Popen(["./google.exe", "Facebook", "--debug"],
+        print(cwd)
+        process = subprocess.Popen(["google.exe", "Facebook", "--debug"],
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE,
                                        cwd=os.path.join(cwd,'dist'), shell=True)
