663a34ef18f545e5b946c04009f02bfb9680332e
Tuan-Pham <weekend.2810@gmail.com>
2023-05-04 16:37:43 +0700

    fix: fix search_test.py

--- search_test.py
+++ search_test.py
@@ -10,18 +10,31 @@ class GoogleTest(unittest.TestCase):
     def test_build_success(self):
         # Change the current working directory
         cwd = os.getcwd()
-        result = subprocess.run(["google.exe", "--help"], capture_output=True, cwd=os.path.join(cwd,'dist'), shell=True)
+        process = subprocess.Popen(["google.exe", "--help"],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       cwd=os.path.join(cwd,'dist'), shell=True)
+
+        result, err = process.communicate()
+
         with open('./test_example/help.txt', 'r') as f:
             help_ref = f.read()
-        self.assertEqual(help_ref.lower(), str(result.stdout).lower())
+        self.assertEqual(help_ref.lower(), str(result).lower())
 
     def test_search_wiki(self):
         # Change the current working directory
         cwd = os.getcwd()
-        result = subprocess.run(["google.exe", "Facebook", "--debug"], capture_output=True, cwd=os.path.join(cwd,'dist'), shell=True)
+        process = subprocess.Popen(["google.exe", "Facebook", "--debug"],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       cwd=os.path.join(cwd,'dist'), shell=True)
+
+        result, err = process.communicate()
+
         with open('./test_example/Facebook_wiki.txt', 'r') as f:
             facebook_wiki_ref = f.read()
-        self.assertIn(facebook_wiki_ref.lower(), str(result.stdout).lower())
+        self.assertIn(facebook_wiki_ref.lower(), str(result).lower())
+        print(err)
 
 
 def main():
