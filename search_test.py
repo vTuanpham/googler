a2c2000bfc8e5a969714fed636499094f4afeced
Tuan-Pham <weekend.2810@gmail.com>
2023-05-04 16:06:34 +0700

    fix: fix search_test cwd

--- search_test.py
+++ search_test.py
@@ -7,16 +7,17 @@ sys.path.insert(0,r'./') #Add root directory here
 
 class GoogleTest(unittest.TestCase):
 
-    # Change the current working directory
-    cwd = os.getcwd()
-
     def test_build_success(self):
+        # Change the current working directory
+        cwd = os.getcwd()
         result = subprocess.run([".\google.exe", "--help"], capture_output=True, cwd=os.path.join(cwd,'dist'), shell=True)
         with open('./test_example/help.txt', 'r') as f:
             help_ref = f.read()
         self.assertEqual(help_ref.lower(), str(result.stdout).lower())
 
     def test_search_wiki(self):
+        # Change the current working directory
+        cwd = os.getcwd()
         result = subprocess.run([".\google.exe", "Facebook", "--debug"], capture_output=True, cwd=os.path.join(cwd,'dist'), shell=True)
         with open('./test_example/Facebook_wiki.txt', 'r') as f:
             facebook_wiki_ref = f.read()
