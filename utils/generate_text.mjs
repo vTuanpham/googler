532a71154c8db7e183010d1078b93c710dbe468c
Tuan-Pham <weekend.2810@gmail.com>
2023-05-21 16:35:45 +0700

    fix: add missing js file

---
+++ utils/generate_text.mjs
@@ -0,0 +1,12 @@
+import pkg from 'terminal-kit';
+const { terminal: term } = pkg;
+
+
+export async function generate_text(text, delay_in_ms) {
+  term.slowTyping(
+    text+'\n',
+    { flashStyle: term.brightWhite, delay: delay_in_ms },
+    function() {}
+  );
+}
+
