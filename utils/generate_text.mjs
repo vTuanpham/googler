91a739ae2bf10bcc23a7f6adeea3ce05aacf230d
Tuan-Pham <weekend.2810@gmail.com>
2023-05-23 20:58:45 +0700

    feat: add slow_typing and syntax highlight for coding disscussion form

--- utils/generate_text.mjs
+++ utils/generate_text.mjs
@@ -6,7 +6,7 @@ export async function generate_text(text, delay_in_ms) {
   term.slowTyping(
     text+'\n',
     { flashStyle: term.brightWhite, delay: delay_in_ms },
-    function() {}
-  );
+	function() {}
+) ;
 }
 
